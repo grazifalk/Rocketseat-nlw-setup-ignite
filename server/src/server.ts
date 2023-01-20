d7be8ce5b3f88c8ad659ea162d9faeb64c2d4e1c
Grazi Falk <83370784+grazifalk@users.noreply.github.com>
2023-01-20 00:15:14 -0300

    Revert "aula 02 - nativewind, icons, renderizar hábitos com cálculo e disposição na tela"
    This reverts commit 4143859e54cda3299a1f4d33ff0e1907213435a2.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { appRoutes } from "./routes";
+import {PrismaClient} from "@prisma/client";
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
 app.register(cors)
-app.register(appRoutes)
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+    return habits
+})
 
 app.listen({
     port: 3333,
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,14 +5,7 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {
-      colors: {
-        background: '#09090A'
-      },
-      gridTemplateRows: {
-        7: 'repeat(7, minmax(0, 1fr))'
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
